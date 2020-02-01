98345f09c1f21a0a0b985688ad55361b697f6e35
Dmitry Anatsko <anatskodr@gmail.com>
2020-02-02 00:20:08 +0300

    Add homework 1

--- lesson4/1.py
+++ lesson4/1.py
@@ -3,4 +3,20 @@
 # Вывести на экран сообщение об этом, а также остаток (если он есть)
 # и частное (в любом случае).
 
-# add code here
+dividend, divisor = None, None
+while dividend is None or divisor is None:
+    try:
+        if dividend is None:
+            dividend = int(input('введите целое делимое число: '))
+        if divisor is None:
+            divisor = int(input('введите целое число делитель: '))
+    except ValueError as exc:
+        print(f'Ошибка: \033[31m{exc}\033[0m, повторите ввод')
+if divisor:
+    print('Первое число делится на второе')
+    remains = dividend % divisor
+    if remains:
+        print('Остаток от деления =', remains)
+    print(dividend, '/', divisor, '=', dividend / divisor)
+else:
+    print('Деление на ноль')
