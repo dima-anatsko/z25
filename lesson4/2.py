b41e115cfa73ddcdf63a9d647e009b08bd7d9343
Dmitry Anatsko <anatskodr@gmail.com>
2020-02-02 00:50:08 +0300

    Add homework 2

--- lesson4/2.py
+++ lesson4/2.py
@@ -1,4 +1,24 @@
 # Заполнить список вещественных чисел вводом с клавиатуры.
 # Сколько элементов списка больше по модулю максимального числа.
 
-# add code here
+items = []
+max_item = None
+col = 0
+while True:
+    number = input('введите число: ')
+    if not number:
+        break
+    try:
+        number = float(number)
+    except ValueError:
+        print('Это не число, повторите ввод.')
+    else:
+        items.append(number)
+        if max_item is None or number > max_item:
+            max_item = number
+if items:
+    for item in items:
+        col = col + 1 if abs(item) > max_item else col
+    print('Кол-во чисел в списке больших по модулю чем максимальное =', col)
+else:
+    print('Список не содержит элементов!')
