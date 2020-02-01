518540d90a16e6dd4ab1d33345d65eb86dd2e135
Dmitry Anatsko <anatskodr@gmail.com>
2020-02-02 01:07:28 +0300

    Add homework 3

--- lesson4/3.py
+++ lesson4/3.py
@@ -1,4 +1,28 @@
 # Заполнить список вещественных чисел вводом с клавиатуры.
 # Найте элементы списка, которые меньше среднего арифметического.
 
-# add code here
+items = []
+sum_items = 0
+while True:
+    number = input('введите число: ')
+    if not number:
+        break
+    try:
+        number = float(number)
+    except ValueError:
+        print('Это не число, повторите ввод.')
+    else:
+        items.append(number)
+        sum_items += number
+if items:
+    means = []
+    mean = sum_items / len(items)
+    for item in items:
+        if item < mean:
+            means.append(item)
+    if means:
+        print('элементы списка, которые меньше ср. арифметического', means)
+    else:
+        print('список не содержит элементов, меньше ср. арифметического')
+else:
+    print('Список не содержит элементов!')
