2d8f332df7f6b3b45109705351a3fbf4946c8236
Dmitry Anatsko <anatskodr@gmail.com>
2020-02-26 01:04:40 +0300

    Add task_2

--- lesson11/homework.py
+++ lesson11/homework.py
@@ -14,7 +14,7 @@ class Fibonacci:
         return self
 
     def __next__(self):
-        while self._one <= self._number:
+        if self._one <= self._number:
             temp = self._one
             self._one, self._two = self._two, self._two + self._one
             return temp
@@ -26,6 +26,21 @@ class Fibonacci:
 Напишите класс, объектом которого будет итератор производящий только
 чётные числа до n включительно.
 """
+
+
+class EvenNumber:
+    def __init__(self, number):
+        self._number = number
+        self._start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._start += 2
+        if self._start <= self._number:
+            return self._start
+        raise StopIteration
 
 
 """
@@ -44,6 +59,7 @@ class Fibonacci:
 
 
 if __name__ == '__main__':
-    c = Fibonacci(10)
-    for i in c:
+    for i in Fibonacci(10):
+        print(i)
+    for i in EvenNumber(11):
         print(i)
