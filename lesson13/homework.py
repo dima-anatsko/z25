eb5939958d7c552a56e8f7487a3bf0f7f7553a88
Dmitry Anatsko <anatskodr@gmail.com>
2020-03-07 21:52:53 +0300

    Start task

--- lesson13/homework.py
+++ lesson13/homework.py
@@ -15,3 +15,50 @@ class Test:
 
 и тд
 """
+import psycopg2
+from psycopg2.extras import execute_values
+
+
+class User:
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    def get_all(self):
+        self.cursor.execute("SELECT * FROM app_users;")
+        return self.cursor.fetchall()
+
+    def get_user(self, user_id=None, username=None):
+        result = None
+        if user_id is not None:
+            self.cursor.execute(
+                "SELECT * FROM app_users WHERE id=%s;", (user_id, )
+            )
+            result = self.cursor.fetchall()
+        elif username is not None:
+            self.cursor.execute(
+                "SELECT * FROM app_users WHERE username=%s;", (username, )
+            )
+            result = self.cursor.fetchall()
+        return result
+
+    def create_users(self, *users_list):
+        users_list = [(i, ) for i in users_list]
+        execute_values(
+            self.cursor,
+            "INSERT INTO app_users (username) VALUES %s;",
+            users_list
+        )
+
+
+if __name__ == '__main__':
+    connection = psycopg2.connect(
+        dsn='postgres://admin_my_app:admin_my_app@localhost:5432/my_app'
+    )
+    cursor = connection.cursor()
+    user_db = User(cursor)
+    # user_db.create_users('Vasja', 'Petja', 'Valera')
+    print(user_db.get_all())
+
+    connection.commit()
+
+    connection.close()
